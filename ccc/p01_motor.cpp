65454e469ce0c7a8c6833e3165caa7f875b41376
devcon <devcon@admantium.com>
2021-03-03 17:36:55 +0100

    wip p01: set speed and duration

--- ccc/p01_motor.cpp
+++ ccc/p01_motor.cpp
@@ -23,7 +23,7 @@ enum class MotorDriver {
 
 struct Motor {
   virtual ~Motor() = default;
-  virtual void drive(Direction) = 0;
+  virtual void drive(Direction, unsigned int) = 0;
 };
 
 struct DcMotor : Motor {
@@ -44,24 +44,32 @@ struct DcMotor : Motor {
     std::cout << "Terminating Motor\n";
   };
 
-  void drive(Direction dir) override{
+  void drive(const Direction dir, const unsigned int duration) override{
     switch (dir) {
       case Direction::Forward: {
-        std::cout << "Driving forward... \n";
+        std::cout << "Driving forward... " << this->speed << " for duration " << duration << "ms \n";
       } break;
       case Direction::Backward: {
-        std::cout << "Driving backward...\n";
+        std::cout << "Driving backward..." << this->speed << " for duration " << duration << "ms \n";
       } break;
       case Direction::Stop: {
-        std::cout << "Stopping...\n";
+        std::cout << "Stopping..." << this->speed << " for duration " << duration << "ms \n";
       }
     }  
+  }
+
+
+  bool set_speed(uint speed) {
+    if (speed > 255) throw std::invalid_argument{"Speed can not be greater than 255"};
+    this->speed = speed;
+    return true;
   }
 
   private:
     const MotorDriver motor_driver;
     const int pin_a;
     const int pin_b;
+    uint speed;
 };
 
 int main() {
@@ -69,7 +77,13 @@ int main() {
 
   DcMotor dc1 = DcMotor{MotorDriver::L293D, 15, 16};
 
-  dc1.drive(Direction::Forward);
-  dc1.drive(Direction::Backward);
-  dc1.drive(Direction::Stop);
+  dc1.drive(Direction::Forward, 10);
+  dc1.drive(Direction::Backward, 5);
+  dc1.drive(Direction::Stop, 500);
+
+  dc1.set_speed(200);
+  dc1.drive(Direction::Forward, 10);
+  dc1.drive(Direction::Backward, 55);
+
+
 }
