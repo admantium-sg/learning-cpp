59fd578617ca586c7e5bce9e3c39dc701b2ea6c4
devcon <devcon@admantium.com>
2021-03-13 09:43:38 +0100

    csr 07_lambda

---
+++ cref/07_lambda.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2021 Sebastian Günther 
+ * 
+ * E-Mail: devcon@admantium.com
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+#include <stdio.h>
+#include <stdexcept>
+#include <iostream>
+#include <cstddef>
+
+using namespace std;
+
+int call(int i, function<int (int)> f) {
+  return f(i);
+}
+
+int main(int argc, char* argv[]) {
+  cout << "Number of args: " << argc << endl;
+  for (int i=0; i < argc; i++) {
+    cout << "Arg " << i << ": " << argv[i] << endl;
+  }
+
+  int i = 2;
+  auto square = [](int i){ return i*2; };
+  int k = call(2, square);
+
+  cout << "i: " << i << " square: " << k << endl;
+
+  int j = 42;
+  auto l = [&j](){return j * 2;}();
+
+  cout << "j: " << j << " square: " << l << endl;
+}
