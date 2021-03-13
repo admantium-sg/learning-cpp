3258d21f5e5e9da8932db2f4b39cdc87c0c1c0d3
devcon <devcon@admantium.com>
2021-03-13 20:51:27 +0100

    cs 11 unions

---
+++ cref/11_unions.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2021 Sebastian Günther 
+ * 
+ * E-Mail: devcon@admantium.com
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <stdio.h>
+#include <stdexcept>
+#include <iostream>
+#include <cstddef>
+
+using namespace std;
+
+union Chaos {
+  char txt[5];
+  int i;
+  double d;
+} chaot;
+
+int main(int argc, char* argv[]) {
+  cout << "Number of args: " << argc << endl;
+  for (int i=0; i < argc; i++) {
+    cout << "Arg " << i << ": " << argv[i] << endl;
+  }
+  
+  chaot.i = 0xC0FFEE;
+  cout << "Chaot " << chaot.txt[0] << " " << chaot.txt[1] << endl;
+
+
+}
