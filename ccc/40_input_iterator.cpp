a19c5dde676d43720ac9ab3ef6e91a5eb70586fc
devcon <devcon@admantium.com>
2021-03-16 21:41:26 +0100

    ccc 40 input iterator

---
+++ ccc/40_input_iterator.cpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2021 Sebastian Günther 
+ * 
+ * E-Mail: devcon@admantium.com
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <stdio.h>
+#include <stdexcept>
+#include <iostream>
+#include <cstddef>
+#include <forward_list>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  
+  cout << "Number of args: " << argc << endl;
+  for (int i=0; i < argc; i++) {
+    cout << "Arg " << i << ": " << argv[i] << endl;
+  }
+
+  forward_list<int> primes{1,2,4,5,7};
+
+  auto only_forward = primes.begin();
+  cout << "Primes: " << *only_forward << endl;
+  only_forward++;
+  only_forward++;
+  only_forward++;
+  cout << "Primes: " << *only_forward << endl;
+  only_forward++;
+  only_forward++;
+  // cout << "Primes: " << *only_forward << endl; Seg Fault!
+
+  // only_forward--; Error: can not decrement value e
+
+
+}
