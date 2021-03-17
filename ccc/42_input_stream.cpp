b1e6d92c021615658f839fe751aaa21fbf5d90ae
devcon <devcon@admantium.com>
2021-03-17 18:03:20 +0100

    ccc 42 input streams

---
+++ ccc/42_input_stream.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021 Sebastian Günther 
+ * 
+ * E-Mail: devcon@admantium.com
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <stdio.h>
+#include <stdexcept>
+#include <iostream>
+#include <cstddef>
+#include <regex>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  
+  cout << "Number of args: " << argc << endl;
+  for (int i=0; i < argc; i++) {
+    cout << "Arg " << i << ": " << argv[i] << endl;
+  }
+
+  string input;
+  size_t count;
+
+  cout << "Enter text: " << flush;
+  while (cin >> input) {
+    count++;
+  }
+
+  cout << endl << "You entered: " << count << " words" << endl;
+}
